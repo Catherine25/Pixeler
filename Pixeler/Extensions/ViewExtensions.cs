f8802cf84503bf5a278f7dc81b53dd616f6cd951
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2022-11-01 00:37:43 +0100

    Add extension to get and set grid position for view

---
+++ Pixeler/Extensions/ViewExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Pixeler.Extensions;
+
+public static class ViewExtensions
+{
+    public static Rect GetGridPosition(this View view) => new()
+    {
+        X = Grid.GetColumn(view),
+        Y = Grid.GetRow(view),
+        Width = Grid.GetColumnSpan(view),
+        Height = Grid.GetRowSpan(view)
+    };
+
+    public static void SetGridPosition(this View view, Rect rect)
+    {
+        Grid.SetColumn(view, (int)rect.X);
+        Grid.SetRow(view, (int)rect.Y);
+        Grid.SetColumnSpan(view, (int)rect.Width);
+        Grid.SetRowSpan(view, (int)rect.Height);
+    }
+}
