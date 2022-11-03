32ff91deec23c88c87d0ed04b5b5ef2eaa4cff2f
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2022-11-03 20:35:36 +0100

    Configure selected image

--- Pixeler/Views/ImageConfigurationView.xaml.cs
+++ Pixeler/Views/ImageConfigurationView.xaml.cs
@@ -1,3 +1,4 @@
+using Pixeler.ExtendedViews;
 using Pixeler.Models;
 using Pixeler.Services;
 
@@ -7,27 +8,84 @@ public partial class ImageConfigurationView : ContentView
 {
 	public Action<Bitmap> BitmapSelected;
 
+	private readonly IAudioService _audioService;
     private readonly IImageService _imageService;
-	private readonly ISettings _settings;
+	private readonly TypedGrid<Button> _grid;
+	private Bitmap _bitmap;
 
-    public ImageConfigurationView(ISettings settings,
+    public ImageConfigurationView(IAudioService audioService,
 		IImageService imageService)
 	{
 		InitializeComponent();
 
-		_settings = settings;
+		_audioService = audioService;
         _imageService = imageService;
+		_grid = new TypedGrid<Button>();
 
-		SelectButton.Clicked += SelectButton_Clicked;
-	}
+        SelectButton.Clicked += SelectButton_Clicked;
+    }
 
 	private async void SelectButton_Clicked(object sender, EventArgs e)
 	{
-        var bitmap = await _imageService.GetBitmapFromStorage();
+		_audioService.Play();
 
-		// todo set it in other view
-        bitmap.Size = _settings.BitmapSize;
+        _bitmap = await _imageService.GetBitmapFromStorage();
 
-        BitmapSelected(bitmap);
+		ImageResolutionLabel.IsVisible = true;
+        ImageResolutionValueLabel.Text = $"{_bitmap.Size.Width}x{_bitmap.Size.Height}, {_bitmap.Size.Width * _bitmap.Size.Height} pixels";
+
+		GenerateLevels((int)Math.Min(_bitmap.Size.Width, _bitmap.Size.Height));
+
+		StartButton.IsVisible = true;
+    }
+
+	private void GenerateLevels(int bitmapSize)
+	{
+        int minimumSize = 2;
+        int step = 2;
+        int currentSize = minimumSize;
+		List<Button> availableLevels = new();
+
+		while (currentSize < bitmapSize)
+        {
+			Button button = new()
+			{
+				Text = currentSize.ToString(),
+			};
+
+			button.Clicked += LevelButton_Clicked;
+
+            availableLevels.Add(button);
+
+			currentSize *= step;
+        }
+
+		_grid.Columns = availableLevels.Count;
+
+        for (int i = 0; i < availableLevels.Count; i++)
+			_grid.Add(availableLevels[i], new Point(i, 0));
+
+		Body.Add(_grid.Grid, 0, 4);
+    }
+
+	private void LevelButton_Clicked(object sender, EventArgs e)
+	{
+        foreach (var item in _grid.Children)
+        	item.IsEnabled = true; // MAUI issue here
+
+        var button = (Button)sender;
+		button.IsEnabled = false;
+
+		int size = int.Parse(button.Text);
+        _bitmap.Size = new Size(size, size);
+
+		StartButton.IsEnabled = true;
+        StartButton.Clicked += StartButton_Clicked;
+    }
+
+	private void StartButton_Clicked(object sender, EventArgs e)
+	{
+		_audioService.Play();
+		BitmapSelected(_bitmap);
     }
 }
