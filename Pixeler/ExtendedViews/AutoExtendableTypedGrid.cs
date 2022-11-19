7d2fe4cf24c1b02de33bd286ef0defe31cb92aa9
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2022-11-19 23:40:38 +0100

    Add AutoExtendableTypedGrid

---
+++ Pixeler/ExtendedViews/AutoExtendableTypedGrid.cs
@@ -0,0 +1,17 @@
+﻿namespace Pixeler.ExtendedViews;
+
+public class AutoExtendableTypedGrid<T> : TypedGrid<T>
+    where T : IView
+{
+    public void AddToRight(T view)
+    {
+        Add(view, new Point(Columns, Rows));
+        Columns++;
+    }
+
+    public void AddToBottom(T view)
+    {
+        Add(view, new Point(Columns, Rows));
+        Rows++;
+    }
+}
