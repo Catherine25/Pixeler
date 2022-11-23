be2f7e3cd6b2c52d884b6aa985ab261053001696
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2022-11-23 16:25:21 +0100

    Add ColoringFuncService

---
+++ Pixeler/Services/ColoringFuncService.cs
@@ -0,0 +1,27 @@
+﻿using Pixeler.Models.Colors;
+using Pixeler.Views;
+
+namespace Pixeler.Services
+{
+    public static class ColoringFuncService
+    {
+        /// <summary>
+        /// 1st - extisting
+        /// 2nd - current selected
+        /// 3rd - resulting
+        /// </summary>
+        /// <param name="modes"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public static Func<ColorData, ColorData, ColorData> GetForMode(Modes modes)
+        {
+            if (modes == Modes.Direct)
+                return (extisting, selected) => selected;
+
+            if(modes == Modes.LayeredBigToSmall_Acryllic) // todo check
+                return (extisting, selected) => extisting.L >= selected.L ? selected : null;
+
+            throw new NotSupportedException();
+        }
+    }
+}
