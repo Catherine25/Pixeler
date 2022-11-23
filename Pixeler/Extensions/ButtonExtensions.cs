19b78bff535e7c8f0eb0b697625c5e185edd61b0
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2022-11-23 01:11:15 +0100

    Add ButtonExtension to attach click sound

---
+++ Pixeler/Extensions/ButtonExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Pixeler.Services;
+
+namespace Pixeler.Extensions;
+
+public static class ButtonExtensions
+{
+    public static void SetClickSound(this Button button, IAudioService audioService)
+    {
+        button.Clicked += (_, _) => audioService.Play();
+    }
+}
