a2e0743bfef905101ebc8ba949f9e15dee2a10d8
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2023-01-12 20:34:53 +0100

    Refactor

--- Pixeler/Services/ColoringFuncService.cs
+++ Pixeler/Services/ColoringFuncService.cs
@@ -5,6 +5,18 @@ namespace Pixeler.Services
 {
     public static class ColoringFuncService
     {
+        private static Dictionary<Modes, Func<ColorData, ColorData, ColorData>> ModeToColorerFunc = new Dictionary<Modes, Func<ColorData, ColorData, ColorData>>
+        {
+            {
+                Modes.Direct,
+                (extisting, selected) => DirectColorerFunc(extisting, selected)
+            },
+            {
+                Modes.LayeredBigToSmall_Acryllic,
+                (extisting, selected) => LayeredBigToSmallAcryllic_ColorerFunc(extisting, selected)
+            }
+        };
+
         /// <summary>
         /// 1st - extisting
         /// 2nd - current selected
@@ -13,15 +25,10 @@ namespace Pixeler.Services
         /// <param name="modes"></param>
         /// <returns></returns>
         /// <exception cref="NotSupportedException"></exception>
-        public static Func<ColorData, ColorData, ColorData> GetForMode(Modes modes)
-        {
-            if (modes == Modes.Direct)
-                return (extisting, selected) => selected;
+        public static Func<ColorData, ColorData, ColorData> GetForMode(Modes modes) => ModeToColorerFunc[modes];
 
-            if(modes == Modes.LayeredBigToSmall_Acryllic) // todo check
-                return (extisting, selected) => extisting.L >= selected.L ? selected : null;
+        private static ColorData DirectColorerFunc(ColorData extisting, ColorData selected) => selected;
 
-            throw new NotSupportedException();
-        }
+        private static ColorData LayeredBigToSmallAcryllic_ColorerFunc(ColorData extisting, ColorData selected) => extisting.L >= selected.L ? selected : null;
     }
 }
