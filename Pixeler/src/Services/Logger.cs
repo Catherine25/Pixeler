1953b6d54eaa1c536c26d22b8147c855564a5167
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2023-03-08 11:26:21 +0100

    Add TimedDelegate logging

--- Pixeler/src/Services/Logger.cs
+++ Pixeler/src/Services/Logger.cs
@@ -6,4 +6,14 @@ public static class Logger
 {
     public static void Log<T>(this T _, string message) => Debug.WriteLine("\t" + message, typeof(T).Name);
     public static void Log<T>(this T classToLog, object o) => Log(classToLog, o.ToString());
+    public static void TimedDelegate<T>(this T classToLog, Action action, string methodName)
+    {
+        var start = DateTime.Now;
+
+        action();
+
+        var end = DateTime.Now;
+
+        classToLog.Log($"{methodName} execution took {end - start}");
+    }
 }
