170250b519a44a435595e4c2f62420b83b66dabd
Catherine Solodukhina <catherinesolodoukhina@gmail.com>
2023-03-08 11:26:46 +0100

    Adjust range

--- Pixeler/src/Models/Settings.cs
+++ Pixeler/src/Models/Settings.cs
@@ -9,7 +9,7 @@ public class Settings : ISettings
     public Size BitmapSize => new(10, 10);
     public int PaletteSize => 10;
     public string SoundPath => "demo-2.wav";
-    public (int Min, int Max) LevelSizeRange { get => (2, 8); }
+    public (int Min, int Max) LevelSizeRange { get => (2, 16); }
     public int PixelHighlightThickness { get => 1; }
     public int PixelMargin { get => 1; }
 }
