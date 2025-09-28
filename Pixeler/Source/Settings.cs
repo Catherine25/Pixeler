b81cec892db16c1f54fa4583ffbf2b71718f34f1
Catherine <catherinesolodoukhina@gmail.com>
2025-09-28 22:42:33 +0200

    Increase level size range

--- Pixeler/Source/Settings.cs
+++ Pixeler/Source/Settings.cs
@@ -9,7 +9,7 @@ public class Settings : ISettings
     public Size BitmapSize => new(10, 10);
     public int PaletteSize => 10;
     public string SoundPath => "demo-2.wav";
-    public (int Min, int Max) LevelSizeRange { get => (2, 16); }
+    public (int Min, int Max) LevelSizeRange { get => (2, 32); }
     public int PixelHighlightThickness { get => 2; }
     public int PixelMargin { get => 2; }
 }
